4f06d0e50c08ef64e3cdb7aa184a1fc4cf054b7c
Pavel <pavel.9794@mail.ru>
2023-06-05 18:39:58 +0300

    Added files

---
+++ src/main/java/Controller/Counter.java
@@ -0,0 +1,18 @@
+package Controller;
+
+public class Counter implements AutoCloseable {
+
+    static int sum;
+    static {
+        sum = 0;
+    }
+
+    public void add() {
+        sum++;
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Выход");
+    }
+}
